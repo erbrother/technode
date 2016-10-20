9c89bfc4455a6e429180aa76ee8acf1a2807faaf
erbrother <z15625129961@163.com>
2016-10-20 17:52:08 +0800

    building...

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/static')));
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '/static/index.html'))
+});
+
+var io = require('socket.io')(server);
+var messages= [];
+
+io.on('connection', function(socket) {
+	console.log('a user login');
+
+	socket.on('getAllMessages', function() {
+		socket.emit('AllMessages', messages)
+	})	
+
+	socket.on('createMessages', function(message) {
+		messages.push(message)
+		io.sockets.emit('messageAdded', message)
+	})
+})
+
+server.listen(port, function() {
+	console.log('listen at port:' + port )
+});
---
+++ static/directives/auto-scroll-to-bottom.js
@@ -0,0 +1,17 @@
+// 指令 
+angular.module('technodeApp').directive('autoScrollToBottom', function() {
+	return {
+		link: function(scope, element, attrs) {
+			scope.$watch(
+				function() {
+					return element.children().length
+				},
+				function() {
+					element.animate({
+						scrollTop: element.prop('scrollHeight')
+					}, 1000)
+				}
+			);
+		}
+	};
+});
